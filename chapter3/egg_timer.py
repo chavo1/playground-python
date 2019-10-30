1e60315d7a1ccb98f10036e35b9bca2c532dc573
chavo1 <chavdar.rakov@gmail.com>
2019-10-30 10:10:21 +0200

    add-ch3

---
+++ chapter3/egg_timer.py
@@ -0,0 +1,4 @@
+import time
+print('Put the egg in boiling water now')
+time.sleep(300)
+print('Take the egg out now')
---
+++ chapter3/firstprog.py
@@ -0,0 +1,3 @@
+print('The answer is: ')
+print(2+2)
+print('The answer is:', 2+2)
